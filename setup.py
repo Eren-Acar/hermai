d0acd64b664088d6a4d22fec426e8df0bc611705
Sadi Evren SEKER <bk@shedai.net>
2025-10-02 07:19:36 +0000

    fix: Add MANIFEST.in to include non-py files in package

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
-# setup.py
+# setup.py (GÜNCELLENMİŞ HALİ)
 
 from setuptools import setup, find_packages
 
 # requirements.txt dosyasını oku
-with open('requirements.txt') as f:
+with open('requirements.txt', 'r', encoding='utf-8') as f:
     required = f.read().splitlines()
+
+# README.md dosyasını oku
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='hermai',
-    version='0.1.0',  # First version
-    author='Sadi Evren SEKER',
-    author_email='hermai@sadievrenseker.com',
+    version='0.1.2',  # Versiyonu güncelledim
+    author='bilgisayarkavramlari', # Adınızı veya kullanıcı adınızı yazın
+    author_email='[E-posta Adresiniz]',
     description='A hermeneutic approach to explainable AI (XAI) with contextual and narrative explanations.',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/bilgisayarkavramlari/hermai', 
-    packages=find_packages(), 
-    install_requires=required, #
+    packages=find_packages(),
+    install_requires=required,
+    include_package_data=True,  # <-- EKLENDİ: MANIFEST.in dosyasını kullan
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
